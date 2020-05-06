3d3fde818d7c58c89f04d8b46b1d00ef97909a69
Jason Vance <jbvance@gmail.com>
2020-05-06 11:13:25 -0500

    add token transfers and testing

--- src/contracts/Token.sol
+++ src/contracts/Token.sol
@@ -1,13 +1,30 @@
 pragma solidity ^0.5.0;
 
+import "openzeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Token {
-    string public name = "Dapp Token";
-    string public symbol = "DAPP";
-    uint256 public decimals = 18;
-    uint256 public totalSupply;
+	using SafeMath for uint;
+	// Variables
+	string public name = "Dapp Token";
+	string public symbol = "DAPP";
+	uint256 public decimals = 18;
+	uint256 public totalSupply;
+	mapping(address => uint256) public balanceOf;
 
-    constructor() public {
-        totalSupply = 1000000 * (10 ** decimals);
-    }
+	// Events
+	event Transfer(address indexed from, address indexed to, uint256 value);
+
+	constructor() public {
+		totalSupply = 1000000 * (10 ** decimals);
+		balanceOf[msg.sender] = totalSupply;
+	}
+
+	function transfer(address _to, uint256 _value)  public returns (bool success) {
+		require(_to != address(0), "Invalid Recipient Address");
+		require(balanceOf[msg.sender] >= _value, "Insufficient Balance");		
+		balanceOf[msg.sender] = balanceOf[msg.sender].sub(_value);
+		balanceOf[_to] = balanceOf[_to].add(_value);
+		emit Transfer(msg.sender, _to, _value);
+		return true;
+	}
 }
--- test/Token.test.js
+++ test/Token.test.js
@@ -1,14 +1,16 @@
+import { EVM_REVERT, tokens } from './helpers';
+
 const Token = artifacts.require('./Token');
 require('chai')
   .use(require('chai-as-promised'))
   .should();
 
-contract('Token', (accounts) => {
+contract('Token', ([deployer, receiver]) => {
   let token;
   const name = 'Dapp Token';
   const symbol = 'DAPP';
   const decimals = '18';
-  const totalSupply = '1000000000000000000000000';
+  const totalSupply = tokens(1000000).toString();
 
   beforeEach(async () => {
     token = await Token.new();
@@ -32,7 +34,60 @@ contract('Token', (accounts) => {
 
     it('tracks the total supply', async () => {
       const result = await token.totalSupply();
-      result.toString().should.equal(totalSupply);
+      result.toString().should.equal(totalSupply.toString());
     });
+
+    it('assigns total supply to the deployer', async () => {
+      const result = await token.balanceOf(deployer);
+      result.toString().should.equal(totalSupply.toString());
+    });
+  });
+
+  describe('sending tokens', () => {
+    let amount;
+    let result;
+
+    describe('success', async () => {
+      beforeEach(async () => {
+        amount = tokens(100);
+        result = await token.transfer(receiver, amount, { from: deployer });
+      });
+
+      it('transfers token balances', async () => {
+        let balanceOf;
+        balanceOf = await token.balanceOf(deployer);
+        balanceOf.toString().should.equal(tokens(999900).toString());
+        balanceOf = await token.balanceOf(receiver);
+        balanceOf.toString().should.equal(tokens(100).toString());
+      });
+
+      it('emits a transfer event', async () => {
+        const log = result.logs[0];
+        log.event.should.eq('Transfer');
+        const event = log.args;
+        event.from.toString().should.equal(deployer, 'from is correct');
+        event.to.toString().should.equal(receiver, 'to is correct');
+        event.value
+          .toString()
+          .should.equal(amount.toString(), 'value is correct');
+      });
+    });
+
+    describe('failure', async () => {
+      it ('rejects insufficient balances', async () => {
+        let invalidAmount;
+        invalidAmount = tokens(100000000) // greater than totalSupply
+        await token.transfer(receiver, invalidAmount, { from: deployer }).should.be.rejectedWith(EVM_REVERT);
+
+        invalidAmount = tokens(10); // sender has no tokens
+        await token.transfer(deployer, invalidAmount, { from: receiver }).should.be.rejectedWith(EVM_REVERT);
+      });
+
+      it ('rejects invalid recipients', async () => {
+        await token.transfer(0x0, amount, {from: deployer }).should.be.rejected;
+      });
+
+    });
+
   });
 });
